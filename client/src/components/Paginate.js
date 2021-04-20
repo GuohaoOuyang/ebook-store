884663280ee53d11737bd6c8ac0e8d98a7cbf2be
Guohao Ouyang <xou96@my.yorku.ca>
2021-04-20 15:39:22 -0400

    refactor paginate

--- client/src/components/Paginate.js
+++ client/src/components/Paginate.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import { Pagination } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
+import React from "react";
+import { Pagination } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import styled from "styled-components";
 
-const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
+const StyledPaginate = styled(Pagination)`
+  margin: 0;
+  .page-item.active .page-link {
+    background-color: ${(props) => props.theme.palette.lightblack};
+    color: white;
+    font-family: "Metropolis";
+  }
+  .page-item {
+    margin: 0.8em;
+  }
+  .page-link {
+    border: none;
+    color: black;
+    border-radius: 10%;
+    &:hover {
+      background-color: transparent;
+    }
+  }
+`;
+
+const Paginate = ({ pages, page, isAdmin = false, keyword = "" }) => {
   return (
     pages > 1 && (
-      <Pagination className='justify-content-center'>
+      <StyledPaginate className="justify-content-center">
         {[...Array(pages).keys()].map((x) => (
           <LinkContainer
             key={x + 1}
@@ -15,14 +36,15 @@ const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
                   ? `/search/${keyword}/page/${x + 1}`
                   : `/page/${x + 1}`
                 : `/admin/productlist/${x + 1}`
-            }
-          >
-            <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
+            }>
+            <Pagination.Item className="shadow-none " active={x + 1 === page}>
+              {x + 1}
+            </Pagination.Item>
           </LinkContainer>
         ))}
-      </Pagination>
+      </StyledPaginate>
     )
-  )
-}
+  );
+};
 
-export default Paginate
+export default Paginate;
